553c84566017d8ea86d5df899c53a443f709c977
TJ Miller <sixlive@users.noreply.github.com>
2019-09-04 11:24:30 -0400

    Adds controller and action to the context (#31)
    * Adds controller and action to the context
    
    * Updates implementation based on https://github.com/honeybadger-io/honeybadger-php/pull/87
    
    * Added changelog

---
+++ src/Middleware/HoneybadgerContext.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Honeybadger\HoneybadgerLaravel\Middleware;
+
+use Closure;
+use Honeybadger\Contracts\Reporter;
+use Illuminate\Support\Facades\Route;
+
+class HoneybadgerContext
+{
+    /**
+     * @var \Honeybadger\Honeybadger;
+     */
+    protected $honeybadger;
+
+    /**
+     * @param  \Honeybadger\Contracts\Reporter  $honeybadger
+     */
+    public function __construct(Reporter $honeybadger)
+    {
+        $this->honeybadger = $honeybadger;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (app()->bound('honeybadger')) {
+            $this->setUserContext($request);
+            $this->setRouteActionContext();
+        }
+
+        return $next($request);
+    }
+
+    private function setRouteActionContext()
+    {
+        if (Route::getCurrentRoute()) {
+            $routeAction = explode('@', Route::getCurrentRoute()->getActionName());
+
+            $this->honeybadger->setComponent($routeAction[0] ?? null);
+            $this->honeybadger->setAction($routeAction[1] ?? null);
+        }
+    }
+
+    private function setUserContext($request)
+    {
+        if ($request->user()) {
+            $this->honeybadger->context(
+                'user_id',
+                $request->user()->getAuthIdentifier()
+            );
+        }
+    }
+}
--- src/Middleware/UserContext.php
+++ src/Middleware/UserContext.php
@@ -2,40 +2,7 @@
 
 namespace Honeybadger\HoneybadgerLaravel\Middleware;
 
-use Closure;
-use Honeybadger\Contracts\Reporter;
-
-class UserContext
+class UserContext extends HoneybadgerContext
 {
-    /**
-     * @var \Honeybadger\Honeybadger;
-     */
-    protected $honeybadger;
-
-    /**
-     * @param  \Honeybadger\Contracts\Reporter  $honeybadger
-     */
-    public function __construct(Reporter $honeybadger)
-    {
-        $this->honeybadger = $honeybadger;
-    }
-
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        if (app()->bound('honeybadger') && $request->user()) {
-            $this->honeybadger->context(
-                'user_id',
-                $request->user()->getAuthIdentifier()
-            );
-        }
-
-        return $next($request);
-    }
+    // Backwards Compatibility
 }
