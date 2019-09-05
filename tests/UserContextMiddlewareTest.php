db67b5ddb1122b22174b717476cb5ddd6d3a1412
TJ Miller <sixlive@users.noreply.github.com>
2019-09-05 07:48:58 -0400

    Add a check to ensure component and action are set (#35)
    * Add a check to ensure context and action are set

--- tests/UserContextMiddlewareTest.php
+++ tests/UserContextMiddlewareTest.php
@@ -2,9 +2,12 @@
 
 namespace Honeybadger\Tests;
 
+use Honeybadger\Honeybadger;
 use Illuminate\Http\Request;
 use Honeybadger\Contracts\Reporter;
+use Illuminate\Support\Facades\Route;
 use Honeybadger\HoneybadgerLaravel\Middleware\UserContext;
+use Honeybadger\HoneybadgerLaravel\Middleware\HoneybadgerContext;
 
 class UserContextMiddlewareTest extends TestCase
 {
@@ -33,5 +36,49 @@ class UserContextMiddlewareTest extends TestCase
         $middleware->handle($request, function () {
             //
         });
+    }
+
+    /** @test */
+    public function it_sets_action_and_context()
+    {
+        $honeybadger = $this->createMock(Honeybadger::class);
+
+        $honeybadger->expects($this->once())
+            ->method('setComponent')
+            ->with('Honeybadger\Tests\Fixtures\TestController');
+
+        $honeybadger->expects($this->once())
+            ->method('setAction')
+            ->with('index');
+
+        $this->app[Reporter::class] = $honeybadger;
+
+        Route::middleware(HoneybadgerContext::class)
+            ->namespace('Honeybadger\Tests\Fixtures')
+            ->group(function () {
+                Route::get('test', 'TestController@index');
+            });
+
+        $this->get('test');
+    }
+
+    /** @test */
+    public function it_does_not_set_action_and_context()
+    {
+        $honeybadger = $this->createMock(Honeybadger::class);
+
+        $honeybadger->expects($this->never())
+            ->method('setComponent');
+
+        $honeybadger->expects($this->never())
+            ->method('setAction');
+
+        $this->app[Reporter::class] = $honeybadger;
+
+        Route::get('test', function () {
+            return response(null, 200);
+        })->middleware(HoneybadgerContext::class);
+
+        $this->get('test');
     }
 }
