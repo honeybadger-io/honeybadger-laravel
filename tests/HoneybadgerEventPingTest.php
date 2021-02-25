bb58c58bb41ce9970cb77717bd7852505a70aee6
Johan <johan@devlop.se>
2021-02-25 11:26:36 +0700

    add tests for pingHoneybadgerOnSuccess

--- tests/HoneybadgerEventPingTest.php
+++ tests/HoneybadgerEventPingTest.php
@@ -72,8 +72,39 @@ class HoneybadgerEventPingTest extends TestCase
     }
 
     /** @test */
+    public function successful_tasks_will_ping_honeybadger()
+    {
+        if (version_compare($this->app->version(), '8.6.0', '<')) {
+            $this->markTestSkipped("Laravel < 8.6 doesn't set proper return codes for callables.");
+
+            return;
+        }
+
+        $schedule = $this->app[Schedule::class];
+
+        $honeybadger = $this->createMock(Reporter::class);
+        $honeybadger->expects($this->once())
+            ->method('checkin')
+            ->with('1234');
+
+        $this->app->instance(Reporter::class, $honeybadger);
+
+        $schedule->call(function () {
+            return true;
+        })->pingHoneybadgerOnSuccess('1234');
+
+        $this->artisan('schedule:run');
+    }
+
+    /** @test */
     public function successful_tasks_will_ping_honeybadger_if_matching_environments()
     {
+        if (version_compare($this->app->version(), '8.6.0', '<')) {
+            $this->markTestSkipped("Laravel < 8.6 doesn't set proper return codes for callables.");
+
+            return;
+        }
+
         $schedule = $this->app[Schedule::class];
 
         $honeybadger = $this->createMock(Reporter::class);
@@ -97,6 +128,12 @@ class HoneybadgerEventPingTest extends TestCase
     /** @test */
     public function successful_tasks_will_not_ping_honeybadger_if_non_matching_environments()
     {
+        if (version_compare($this->app->version(), '8.6.0', '<')) {
+            $this->markTestSkipped("Laravel < 8.6 doesn't set proper return codes for callables.");
+
+            return;
+        }
+
         $schedule = $this->app[Schedule::class];
 
         $honeybadger = $this->createMock(Reporter::class);
