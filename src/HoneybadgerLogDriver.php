453985288ede4bd30c12ab8c1c6513b9ea6f6eb9
shalvah <diakon.ng@gmail.com>
2021-06-24 19:51:51 +0100

    Fix style

--- src/HoneybadgerLogDriver.php
+++ src/HoneybadgerLogDriver.php
@@ -2,14 +2,13 @@
 
 namespace Honeybadger\HoneybadgerLaravel;
 
-use Monolog\Logger;
 use Honeybadger\LogHandler;
-use Honeybadger\Contracts\Reporter;
 use Illuminate\Support\Facades\App;
+use Monolog\Logger;
 
 class HoneybadgerLogDriver
 {
-    public function __invoke(array $config) : Logger
+    public function __invoke(array $config): Logger
     {
         return tap(new Logger($config['name'] ?? 'honeybadger'), function ($logger) {
             $logger->pushHandler(App::make(LogHandler::class));
