b1d903ef38097394023af635ff34d7f1d682cd90
Chris Di Carlo <chris@dicarlosystems.ca>
2023-03-17 15:48:55 -0400

    Add tests for macros

--- tests/HoneybadgerEventPingTest.php
+++ tests/HoneybadgerEventPingTest.php
@@ -4,6 +4,7 @@ namespace Honeybadger\Tests;
 
 use Honeybadger\Contracts\Reporter;
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Config;
 
 class HoneybadgerEventPingTest extends TestCase
 {
@@ -67,6 +68,27 @@ class HoneybadgerEventPingTest extends TestCase
         $schedule->call(function () {
             return true;
         })->thenPingHoneybadger('1234', ['development', 'production']);
+
+        $this->artisan('schedule:run');
+    }
+
+       /** @test */
+    public function scheduled_tasks_will_ping_honeybadger_with_id_from_config()
+    {
+        $schedule = $this->app[Schedule::class];
+
+        Config::set('test_id', '1234');
+
+        $honeybadger = $this->createMock(Reporter::class);
+        $honeybadger->expects($this->once())
+            ->method('checkin')
+            ->with('1234');
+
+        $this->app->instance(Reporter::class, $honeybadger);
+
+        $schedule->call(function () {
+            return true;
+        })->thenPingHoneybadgerFromConfig('test_id');
 
         $this->artisan('schedule:run');
     }
@@ -149,6 +171,33 @@ class HoneybadgerEventPingTest extends TestCase
         $schedule->call(function () {
             return true;
         })->pingHoneybadgerOnSuccess('1234', ['development', 'production']);
+
+        $this->artisan('schedule:run');
+    }
+
+    /** @test */
+    public function successful_tasks_will_ping_honeybadger_with_id_from_config()
+    {
+        if (version_compare($this->app->version(), '8.6.0', '<')) {
+            $this->markTestSkipped("Laravel < 8.6 doesn't set proper return codes for callables.");
+
+            return;
+        }
+
+        Config::set('test_id', '1234');
+
+        $schedule = $this->app[Schedule::class];
+
+        $honeybadger = $this->createMock(Reporter::class);
+        $honeybadger->expects($this->once())
+            ->method('checkin')
+            ->with('1234');
+
+        $this->app->instance(Reporter::class, $honeybadger);
+
+        $schedule->call(function () {
+            return true;
+        })->pingHoneybadgerOnSuccessFromConfig('test_id');
 
         $this->artisan('schedule:run');
     }
