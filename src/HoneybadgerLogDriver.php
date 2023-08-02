7f67bcf77e8adf900eac2d2913062b72f66c5052
shalvah <diakon.ng@gmail.com>
2023-08-02 21:38:41 +0200

    LogDriver: Pass error level to LogHandler

--- src/HoneybadgerLogDriver.php
+++ src/HoneybadgerLogDriver.php
@@ -2,16 +2,20 @@
 
 namespace Honeybadger\HoneybadgerLaravel;
 
+use Honeybadger\Contracts\Reporter;
 use Honeybadger\LogHandler;
 use Illuminate\Support\Facades\App;
-use Monolog\Logger;
 
 class HoneybadgerLogDriver
 {
     public function __invoke(array $config): Logger
     {
         return tap(new Logger($config['name'] ?? 'honeybadger'), function ($logger) {
-            $logger->pushHandler(App::make(LogHandler::class));
+            $logHandler = App::makeWith(LogHandler::class, [
+                App::make(Reporter::class),
+                $config['level'] ?? 'error'
+            ]);
+            $logger->pushHandler($logHandler);
         });
     }
 }
