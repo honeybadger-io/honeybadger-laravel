51933cb3f38e4ab8c39d07e31b1853551aac2148
Pangratios Cosma <pangratios.cosma@honeybadger.io>
2024-09-06 12:39:59 +0300

    chore: fix tests

--- tests/AutomaticBreadcrumbsTest.php
+++ tests/AutomaticBreadcrumbsTest.php
@@ -138,6 +138,10 @@ class AutomaticBreadcrumbsTest extends TestCase
     /** @test */
     public function adds_breadcrumbs_for_views()
     {
+        $this->withHeaders([
+            'X-Request-Id' => '1234',
+        ]);
+
         Config::set('honeybadger.breadcrumbs.automatic', [ViewRendered::class]);
         Config::set('view.paths', [realpath(__DIR__.'/Fixtures/views')]);
         Route::get('test', function () {
@@ -150,6 +154,7 @@ class AutomaticBreadcrumbsTest extends TestCase
             ->with('View rendered', [
                 'name' => 'test',
                 'path' => realpath(__DIR__.'/Fixtures/views').'/test.blade.php',
+                'requestId' => '1234',
             ], 'render');
 
         $this->app->instance(Reporter::class, $honeybadger);
