e867ebeb588b1759fafd876594af2cb8ce28fe49
Pangratios Cosma <pangratios.cosma@honeybadger.io>
2024-09-14 17:33:42 +0300

    feat: duration for ViewRendered

--- src/Events/ViewRendered.php
+++ src/Events/ViewRendered.php
@@ -2,11 +2,14 @@
 
 namespace Honeybadger\HoneybadgerLaravel\Events;
 
+use Illuminate\Events\Dispatcher;
 use Illuminate\Support\Facades\View;
 
 class ViewRendered extends ApplicationEvent
 {
     public string $handles = 'composing:*';
+
+    private float $startTime;
 
     /**
      * @param string $event
@@ -21,6 +24,7 @@ class ViewRendered extends ApplicationEvent
             $metadata = [
                 'name' => $view->getName(),
                 'path' => $view->getPath(),
+                'duration' => $this->getDurationInMs($this->startTime),
             ];
         } else {
             // $eventName is sometimes "composing: errors::500" with no $data
@@ -28,6 +32,7 @@ class ViewRendered extends ApplicationEvent
             $metadata = [
                 'name' => $name,
                 'path' => View::getFinder()->find($name),
+                'duration' => $this->getDurationInMs($this->startTime),
             ];
         }
 
@@ -37,5 +42,16 @@ class ViewRendered extends ApplicationEvent
             'View rendered',
             $metadata,
         );
+    }
+
+    public function register(): void {
+        /** @var Dispatcher $dispatcher */
+        $dispatcher = app('events');
+
+        $dispatcher->listen("creating:*", function ($event) {
+            $this->startTime = microtime(true);
+        });
+
+        $dispatcher->listen($this->handles, [$this, 'handle']);
     }
 }
