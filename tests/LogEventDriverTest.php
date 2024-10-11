fdda4e9b90dc47f8c17844909a1a1a5ac2e4ffa9
Pangratios Cosma <pangratios.cosma@honeybadger.io>
2024-10-11 12:06:34 +0300

    chore: update honeybadger-php package

--- tests/LogEventDriverTest.php
+++ tests/LogEventDriverTest.php
@@ -21,21 +21,82 @@ class LogEventDriverTest extends TestCase
             'name' => 'asdf', // optional point of customization
         ]);
 
-        $reporter = $this->createMock(Reporter::class);
-
+        $reporter = $this->getMockReporter();
         $logHandler = new LogEventHandler($reporter);
-
-        $reporter->expects($this->once())
-            ->method('event')
-            ->with('log', [
-                'ts' => (new \DateTime())->format(DATE_ATOM),
-                'severity' => 'info',
-                'message' => 'Test message',
-                'channel' => 'asdf',
-            ]);
 
         $this->app[LogEventHandler::class] = $logHandler;
 
         Log::channel('honeybadger')->info('Test message');
+
+        $events = $reporter->getEvents();
+        $this->assertCount(1, $events);
+
+        $event = $events[0];
+        $this->assertArrayHasKey('ts', $event[1]);
+        $this->assertEquals('log', $event[0]);
+        $this->assertEquals('info', $event[1]['severity']);
+        $this->assertEquals('Test message', $event[1]['message']);
+        $this->assertEquals('asdf', $event[1]['channel']);
     }
+
+    private function getMockReporter(): Reporter {
+        return new class implements Reporter {
+            private array $events = [];
+
+            public function notify($throwable, $request = null, array $additionalParams = []): array
+            {
+                return [];
+            }
+
+            public function customNotification(array $payload): array
+            {
+                return [];
+            }
+
+            public function rawNotification(callable $callable): array
+            {
+                return [];
+            }
+
+            public function checkin(string $idOrSlug): void
+            {
+            }
+
+            public function context($key, $value = null)
+            {
+            }
+
+            public function addBreadcrumb(string $message, array $metadata = [], string $category = 'custom'): Reporter
+            {
+                return $this;
+            }
+
+            public function clear(): Reporter
+            {
+                return $this;
+            }
+
+            public function event($eventTypeOrPayload, array $payload = null): void
+            {
+                $this->events[] = [$eventTypeOrPayload, $payload];
+            }
+
+            public function flushEvents(): void
+            {
+                $this->events = [];
+            }
+
+            /**
+             * Accessor method to the events array.
+             *
+             * @return array
+             */
+            public function getEvents(): array
+            {
+                return $this->events;
+            }
+        };
+    }
+
+
 }
