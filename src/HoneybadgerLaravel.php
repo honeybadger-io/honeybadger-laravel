8dd5a31f4af6140ccc9d12bf3f608c9b79c48017
subzero10 <subzero10@users.noreply.github.com>
2024-11-01 17:39:41 +0000

    chore: update version

--- src/HoneybadgerLaravel.php
+++ src/HoneybadgerLaravel.php
@@ -12,7 +12,7 @@ use Throwable;
 
 class HoneybadgerLaravel extends Honeybadger
 {
-    const VERSION = '4.2.2';
+    const VERSION = '4.3.0';
 
     const DEFAULT_EVENTS = [
         Events\DatabaseQueryExecuted::class,
